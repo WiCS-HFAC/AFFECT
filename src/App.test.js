8cebbca2704e661f8cf349569b31db58a6ca4b38
kylemh <kylemh.email@gmail.com>
2017-04-08 12:27:55 -0700

    Edit tests and remove unnecessary dependencies

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Home from './components/Home';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Home />, div);
 });
-
-const exec = require('mz/child_process').exec;
-const request = require('supertest-as-promised');
-const expect = require('chai').expect;
 
 const app = require('../server/app');
 
@@ -20,30 +16,5 @@ describe('builds application', function () {
 
     // Run process
     return exec('npm run build');
-  });
-});
-
-describe('express serving', function () {
-  it('responds to / with the index.html', function () {
-    return request(app)
-      .get('/')
-      .expect('Content-Type', /html/)
-      .expect(200)
-      .then(res => expect(res.text).to.contain('<div id="root"></div>'));
-  });
-
-  it('responds to favicon.icon request', function () {
-    return request(app)
-      .get('/favicon.ico')
-      .expect('Content-Type', 'image/x-icon')
-      .expect(200);
-  });
-
-  it('responds to any route with the index.html', function () {
-    return request(app)
-      .get('/foo/bar')
-      .expect('Content-Type', /html/)
-      .expect(200)
-      .then(res => expect(res.text).to.contain('<div id="root"></div>'));
   });
 });
