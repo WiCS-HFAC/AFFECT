74500e1b38f5af8c21d3b17bc5360bc016a504a6
kylemh <kylemh.email@gmail.com>
2017-04-08 03:17:17 -0700

    Update File Structure

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
+import classnames from 'classnames';
+
 import logo from './logo.svg';
-import './App.css';
+import './style.css';
 
 class App extends Component {
+  static propTypes = {}
+  static defaultProps = {}
+  state = {}
+
+
   render() {
+    const { className, ...props } = this.props;
     return (
-      <div className="App">
+      <div className={classnames('App', className)} {...props}>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Hi, so this is really cool!
+          Time to get going!
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { browserHistory } from 'react-router';
+
+import Routes from './routes.js';
+
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Routes history={browserHistory} />,
   document.getElementById('root')
 );
