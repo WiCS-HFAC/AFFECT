4d43e4b5eb9226389d850da783b674ce064aaffc
kylemh <kylemh.email@gmail.com>
2017-04-08 11:43:54 -0700

    Update components

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import App from './components/App';
+import Home from './components/Home';
+import Register from './components/Register';
 import Login from './components/Login';
+import Apply from './components/Apply';
 import NotFound from './components/NotFound';
 
 const Routes = () => (
   <Router>
     <div>
-        <Route path="/" component={App} />
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/register">Register</Link></li>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/apply">Apply</Link></li>
+        </ul>
+
+        <hr/>
+
+        <Route extract path="/" component={Home} />
+        <Route path="/register" component={Register} />
         <Route path="/login" component={Login} />
         <Route path="/404" component={NotFound} />
     </div>
