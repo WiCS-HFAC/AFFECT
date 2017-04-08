3e3a80a868ec838f0faae862d1858e2a0eff677c
kylemh <kylemh.email@gmail.com>
2017-04-08 14:52:17 -0700

    Complete 404 Page

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Link } from 'react';
 import classnames from 'classnames';
 
 import './style.css';
@@ -8,7 +8,9 @@ export default class NotFound extends Component {
     const { className } = this.props;
     return (
       <div className={classnames('NotFound', className)}>
-        <h1>404 <small>Not Found :(</small></h1>
+        <h1>404</h1>
+        <h2>Page not found!</h2>
+        <p><Link to="/">Go back to the home page</Link></p>
       </div>
     );
   }
