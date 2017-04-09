0006264f4f573a88fa7908364c7e8b7fbaac19e1
kylemh <kylemh.email@gmail.com>
2017-04-08 23:29:07 -0700

    Finish navbar

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { Component, PropTypes } from 'react';
+import { BrowserRouter as Router, Route, history } from 'react-router-dom';
 
 import Home from './components/Home';
 import Register from './components/Register';
@@ -14,7 +14,27 @@ import {Tabs, Tab} from 'material-ui';
 
 const muiTheme = getMuiTheme(theme);
 
+const Nav = (props, Comp) => {
+    return (
+        <div>
+            <MuiThemeProvider muiTheme={muiTheme}>
+                <Tabs
+                    onChange={(value) => {
+                        props.history.push(value)
+                    }}>
+                    <Tab label="Home" value="/"  />
+                    <Tab label="Login" value="/login" />
+                    <Tab label="Register" value="/register" />
+                    <Tab label="Apply" value="/apply" />
+                </Tabs>
+            </MuiThemeProvider>
+            <Comp {...props} />
+        </div>
+    )
+}
+
 class Routes extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
@@ -32,22 +52,12 @@ class Routes extends Component {
         return  (
             <Router>
                 <div>
-                <MuiThemeProvider muiTheme={muiTheme}>
-                    <Tabs value={this.state.value} onChange={this.handleChange}>
-                        <Tab label="Home" value="a" />
-                        <Tab label="Login" value="b" />
-                        <Tab label="Register" value="c" />
-                        <Tab label="Apply" value="d" />
-                    </Tabs>
-                </MuiThemeProvider>
-
-                <Route exact path="/" component={Home} />
-                <Route path="/login" component={Login} />
-                <Route path="/register" component={Register} />
-                <Route path="/apply" component={Apply} />
-                <Route path="/404" component={NotFound} />
+                <Route exact path="/" component={(props) => Nav(props, Home)} />
+                <Route path="/login" component={(props) => Nav(props, Login)} />
+                <Route path="/register" component={(props) => Nav(props, Register)} />
+                <Route path="/apply" component={(props) => Nav(props, Apply)} />
+                <Route path="/404" component={(props) => Nav(props, NotFound)} />
                 {/* TODO: Why isnt asterisk acting as a wildcard */}
-
                 </div>
             </Router>
         )
