dc9f779c32a09375b564a894fd3a2311981194c3
kylemh <kylemh.email@gmail.com>
2017-04-08 19:33:49 -0700

    Responsive navbar

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,34 +3,15 @@ import classnames from 'classnames';
 
 import './style.css';
 
-const button_style = {
-    margin: 15,
-    maxWidth: 75,
-    alignSelf: "center",
-}
-const appBar_style = {
-    display: "flex",
-   flexDirection: "row",
-}
-const tField_style = {
-    alignSelf: "center",
-}
-const whole_style = {
-    display: "flex",
-    flexFlow: "column wrap",
-    alignContent: "center",
-}
 
 class Home extends Component {
   render() {
     const { className } = this.props;
     return (
       <div className={classnames('Home', className)}>
-        <div className="Home-header">
+        <div>
           <h1>Home</h1>
         </div>
-
-        <p className="Home-intro"> This is the homepage.</p>
       </div>
     );
   }
--- src/routes.js
+++ src/routes.js
@@ -13,22 +13,30 @@ import NotFound from './components/NotFound';
 
 
 const link_style = {
+    width: "25%",
+    textAlign: "center",
     color: "black",
     display: "inline-block",
-    paddingLeft: "17%",
-    paddingTop: "17px",
-    paddingBottom: "17px"
+    paddingTop: "0.5%",
+    paddingBottom: "0.5%",
+    textDecoration: "None",
+}
+
+const navBar = {
+    borderBottom: "1px lightgrey solid",
 }
 
 
 const Routes = () => (
   <Router>
     <div>
-        <Link to="/" style={link_style}>Home</Link>
-        <Link to="/register" style={link_style}>Register</Link>
-        <Link to="/login" style={link_style}>Login</Link>
-        <Link to="/apply" style={link_style}>Apply</Link>
-        
+        <div class="navBar">
+            <Link to="/" style={link_style}>Home</Link>
+            <Link to="/register" style={link_style}>Register</Link>
+            <Link to="/login" style={link_style}>Login</Link>
+            <Link to="/apply" style={link_style}>Apply</Link>
+        </div>
+
         <Route exact path="/" component={Home} />
         <Route path="/register" component={Register} />
         <Route path="/login" component={Login} />
